4ae4ae0a6c49881ddd2ec5132961c91077c2361e
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-23 17:07:28 +0530

    Temperature conversion of celsius,Fahrenheit

--- Program.cs
+++ Program.cs
@@ -5,24 +5,21 @@ using System.Diagnostics;
 namespace LogicalPrograms
 {
     public class Program
-    {
+    {    
         public static void Main(string[] args)
         {
-            if (args.Length != 3)
+            Console.WriteLine("Enter the temperature:");
+            if (double.TryParse(Console.ReadLine(), out double temperature))
             {
-                Console.WriteLine("Usage: dotnet run <month> <day> <year>");
-                return;
-            }
-            if (int.TryParse(args[0], out int month) &&
-                int.TryParse(args[1], out int day) &&
-                int.TryParse(args[2], out int year))
-            {
-                int dayOfWeek = DayOfWeek.DayOfWeeks(day, month, year);
-                Console.WriteLine($"Day of the week: {dayOfWeek}");
+                Console.WriteLine("Enter 'C' for Celsius to Fahrenheit or 'F' for Fahrenheit to Celsius:");
+                char choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                TemperatureConversion.temperatureConversion(choice, temperature);
             }
             else
             {
-                Console.WriteLine("Invalid input. Please enter valid integers for month, day, and year.");
+                Console.WriteLine("Invalid input. Please enter a valid temperature.");
             }
         }
     }
---
+++ TemperatureConversion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class TemperatureConversion
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public static void temperatureConversion(char choice, double temperature)
+        {
+            if (choice == 'F' || choice == 'f')
+            {
+                double celsius = FahrenheitToCelsius(temperature);
+                Console.WriteLine($"Temperature in Celsius: {celsius:F2} °C");
+            }
+            else if (choice == 'C' || choice == 'c')
+            {
+                double fahrenheit = CelsiusToFahrenheit(temperature);
+                Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit:F2} °F");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter 'C' for Celsius to Fahrenheit or 'F' for Fahrenheit to Celsius.");
+            }
+        }
+    }
+}
