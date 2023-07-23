be67ff010506975e91a8a0a753b4fdf285270f4c
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-23 13:34:39 +0530

    Generate Coupon Number

--- Program.cs
+++ Program.cs
@@ -6,19 +6,8 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("=======**Welcome To Logical Programming Problems**========");
-
-                Console.WriteLine("Enter 2 or more digit a number");
-                int number = Convert.ToInt32(Console.ReadLine());
-                var reverseNumber = new ReverseNumber();
-                reverseNumber.ReverseInputNumber(number);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Please Enter only Number");
-            }
+            Console.WriteLine("=======**Welcome To Logical Programming Problems**========");
+            CouponNumber.GenerateCouponNumber();
         }
     }
 }
