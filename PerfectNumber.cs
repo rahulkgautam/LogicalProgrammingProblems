e6c1d6a38048c43d13236bcd32f7a8c72bd98bc8
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-23 18:24:07 +0530

    Find Perfect Number or not

---
+++ PerfectNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class PerfectNumber
+    {
+        public static long IsPerfect(long num)
+        {
+            long sum = 0;
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine(i);
+                }
+            }
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ using System.Text;
 namespace LogicalPrograms
 {
 
+   
+
     public class Program
     {
-        
         public static void Main(string[] args)
         {
-            ToBinaryConversion.ToBinary();
+            long number, sum;
+            Console.WriteLine("Enter the number: ");
+            number = Convert.ToInt64(Console.ReadLine());
+            sum = PerfectNumber.IsPerfect(number);
+            if (sum == number)
+                Console.WriteLine(number + " is a perfect number");
+            else
+                Console.WriteLine(number + " is not a perfect number");
         }
-    }    
-}
+    }
+ }
